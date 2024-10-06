9309fc8ffc4df01380a5e859810db6946200ed91
pabloescobar2021 <mishutinpavlusha123@gmail.com>
2024-10-06 23:19:15 +0400

    summary

--- js/other.js
+++ js/other.js
@@ -77,21 +77,49 @@ contnvk.addEventListener('mouseleave', () => {
 
 
 
+// --------------------------------------------------
 
 
+// const conttxt = document.querySelectorAll('.intopunder');
+// const txtbutt = document.querySelectorAll('.battxt_1');
 
-const conttxt = document.querySelectorAll('.intopunder');
-const txtbutt = document.querySelectorAll('.battxt_1');
+// // Проходим по каждой кнопке и добавляем обработчик клика
+// txtbutt.forEach((button, index) => {
+//     button.addEventListener('click', function() {
+//         // Переключаем видимость соответствующего элемента
+//         const content = conttxt[index];
+//         if (content.style.display === 'flex') {
+//             content.style.display = 'none';
+//         } else {
+//             content.style.display = 'flex';
+//         }
+//     });
+// });
 
-// Проходим по каждой кнопке и добавляем обработчик клика
-txtbutt.forEach((button, index) => {
-    button.addEventListener('click', function() {
-        // Переключаем видимость соответствующего элемента
-        const content = conttxt[index];
-        if (content.style.display === 'block') {
-            content.style.display = 'none';
-        } else {
-            content.style.display = 'block';
+
+const parag = document.querySelectorAll('.battxt_1');
+
+parag.forEach((p) =>{
+    const arrow = document.createElement('span');
+    arrow.textContent = '˅';
+    arrow.style.cursor = 'pointer';
+    p.appendChild(arrow);
+
+    // Добавляем обработчик событий на клик
+    p.addEventListener('click', function() {
+        // Находим следующий элемент .intopunder
+        const nextContent = p.nextElementSibling;
+        
+        // Проверяем, является ли следующий элемент блоком .intopunder
+        if (nextContent && nextContent.classList.contains('intopunder')) {
+            // Если блок виден, скрываем его, если скрыт — показываем
+            if (nextContent.style.display === 'block') {
+                nextContent.style.display = 'none';
+                arrow.textContent = ' ˅';  // Возвращаем стрелку вниз
+            } else {
+                nextContent.style.display = 'block';
+                arrow.textContent = ' ˄';  // Меняем стрелку на вверх
+            }
         }
     });
-});
+})
